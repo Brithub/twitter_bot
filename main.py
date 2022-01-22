757aa25c0a35980af81113cd41ccc5e903f4935c
sambritton <sambritton212@gmail.com>
2022-01-22 12:03:22 -0600

    added a main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import generate
+import tweet_dumper
+
+
+def downloader_function(thing, thing2):
+    tweet_dumper.downloader_function(thing, thing2)
+
+
+def compose_and_send_tweet(thing, thing2):
+    generate.compose_and_send_tweet(thing, thing2)
