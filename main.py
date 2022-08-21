8620bdff92c65ac9b516df8c76fadc1f457ab308
sambritton <sambritton212@gmail.com>
2022-08-20 21:11:33 -0400

    Seeing if we're just erroring out?

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import pfp
 
 
 def downloader_function(thing, thing2):
-    return tweet_dumper.downloader_function(thing, thing2)
+    try:
+        return tweet_dumper.downloader_function(thing, thing2)
+    except Exception as e:
+        print(f"Something went wrong")
+        return e.with_traceback()
 
 
 def compose_and_send_tweet(thing, thing2):
